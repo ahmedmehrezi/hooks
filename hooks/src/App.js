12ce47b056df2b884c3ace60e3c357c13d831318
ahmedmehrezi <mehreziahmed25@gmail.com>
2022-10-02 00:10:57 +0100

    hooks

---
+++ hooks/src/App.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import './App.css';
+import MovieList from './components/MovieList';
+import { Data } from './Data';
+
+function App() {
+  const [movies,setMovies]=useState(Data)
+  const addMovie =(movie)=>{
+    setMovies([...movies,movie])
+  }
+  const deleteMovie = (id)=>{
+    setMovies(movies.filter((movie)=> movie.id !== id))
+  }
+
+
+  return (
+    <div className="App">
+      <MovieList movies={movies} deleteMovie={deleteMovie} addMovie={addMovie}/>
+      
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ hooks/src/components/MovieCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './components.css'
+import Rating from 'react-star-rating-lite';
+
+export default function MovieCard({movie ,deleteMovie}) {
+  
+  
+  return (
+      
+      <div className='box1'>
+            <h1>{movie.title}</h1>
+            <h1>{movie.description}</h1>
+            <img style={{width:"200px"}} src={movie.posterUrl} alt="" />
+            <h1>{movie.rate}</h1>
+            <button onClick={()=>deleteMovie(movie.id)} >remove card</button>
+            <Rating value={movie.rate} readonly />
+ 
+    </div>
+    
+  )
+}
